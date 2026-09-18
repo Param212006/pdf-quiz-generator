85d46cad777751c37ca6c1a818aaad7d15049088
Param212006 <diwali2026waiting@gmail.com>
2026-09-18 22:05:17 +0000

    Update Groq model ID to supported llama3-8b-8192

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ Text:
 {extracted_text[:2500]}"""
 
         response = client.chat.completions.create(
-            model="llama-3.1-8b-instant",
+            model="llama3-8b-8192",
             messages=[{"role": "user", "content": prompt}],
             temperature=0.2,
             max_tokens=2000
